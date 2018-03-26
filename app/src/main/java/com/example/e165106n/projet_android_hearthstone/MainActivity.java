a909d13afd420b7ec33a2d28df6dbad267e9c14f
ZetaOrionis <migadel.kevin@gmail.com>
2018-03-26 10:02:25 +0200

    First Commit

---
+++ app/src/main/java/com/example/e165106n/projet_android_hearthstone/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.e165106n.projet_android_hearthstone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
